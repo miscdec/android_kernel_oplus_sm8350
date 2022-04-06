8fe2365ec37b86ea203eba37c03aae835c83fc16
LuK1337 <priv.luk@gmail.com>
2022-04-07 01:27:26 +0200

    tfa98xx-v6: Use fixed size buffer instead of VLA

--- techpack/audio/asoc/codecs/tfa98xx-v6/tfa98xx_v6.c
+++ techpack/audio/asoc/codecs/tfa98xx-v6/tfa98xx_v6.c
@@ -497,18 +497,17 @@ static int kernel_debug_open(struct inode *inode, struct file *file)
 static ssize_t kernel_debug_read(struct file *file, char __user *buf,
 				 size_t count, loff_t *pos)
 {
-	/* /sys/kernel/debug/ftm_tfa98xx */
-	const int size = 1024;
-	char buffer[size];
+/* /sys/kernel/debug/ftm_tfa98xx */
+	char buffer[1024];
 	int n = 0;
 
-	n += scnprintf(buffer + n, size - n, "%s ", ftm_load_file);
-	n += scnprintf(buffer + n, size - n, "%s ", ftm_clk);
-	n += scnprintf(buffer + n, size - n, "%s ", ftm_SpeakerCalibration);
-	n += scnprintf(buffer + n, size - n, "%s ", ftm_path);
-	n += scnprintf(buffer + n, size - n, "%s ", ftm_spk_resistance);
-	n += scnprintf(buffer + n, size - n, "%s ", ftm_tfa98xx_flag);
-	n += scnprintf(buffer + n, size - n, "%d ", ftm_mode);
+	n += scnprintf(buffer + n, sizeof(buffer) - n, "%s ", ftm_load_file);
+	n += scnprintf(buffer + n, sizeof(buffer) - n, "%s ", ftm_clk);
+	n += scnprintf(buffer + n, sizeof(buffer) - n, "%s ", ftm_SpeakerCalibration);
+	n += scnprintf(buffer + n, sizeof(buffer) - n, "%s ", ftm_path);
+	n += scnprintf(buffer + n, sizeof(buffer) - n, "%s ", ftm_spk_resistance);
+	n += scnprintf(buffer + n, sizeof(buffer) - n, "%s ", ftm_tfa98xx_flag);
+	n += scnprintf(buffer + n, sizeof(buffer) - n, "%d ", ftm_mode);
 
 	return simple_read_from_buffer(buf, count, pos, buffer, n);
 }
