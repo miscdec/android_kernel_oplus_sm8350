4b8443efb1463991a1275484486bcde1b53828f3
LuK1337 <priv.luk@gmail.com>
2022-04-16 23:53:06 +0200

    oplus_touchscreen_v2: Add software only black gesture indep support
    Change-Id: I94202849678b0e908404468a8e6a71a62745240a

--- drivers/input/touchscreen/oplus_touchscreen_v2/touchpanel_common_driver.c
+++ drivers/input/touchscreen/oplus_touchscreen_v2/touchpanel_common_driver.c
@@ -636,8 +636,9 @@ static void tp_gesture_handle(struct touchpanel_data *ts)
 
 	if (gesture_info_temp.gesture_type != UNKOWN_GESTURE
 	    && gesture_info_temp.gesture_type != FINGER_PRINTDOWN
-	    && gesture_info_temp.gesture_type != FRINGER_PRINTUP) {
-		retval = tp_memcpy(&ts->gesture, sizeof(ts->gesture), \
+	    && gesture_info_temp.gesture_type != FRINGER_PRINTUP
+	    && CHK_BIT(ts->gesture_enable_indep, (1 << gesture_info_temp.gesture_type))) {
+		tp_memcpy(&ts->gesture, sizeof(ts->gesture), \
 			  &gesture_info_temp, sizeof(struct gesture_info), \
 			  sizeof(struct gesture_info));
 		if (retval < 0) {
@@ -1937,8 +1938,7 @@ static int init_parse_dts(struct device *dev, struct touchpanel_data *ts)
 	ts->black_gesture_support   = of_property_read_bool(np,
 				      "black_gesture_support");
 
-	ts->black_gesture_indep_support = of_property_read_bool(np,
-								"black_gesture_indep_support");
+	ts->black_gesture_indep_support = true;
 
 	ts->gesture_test_support    = of_property_read_bool(np,
 				      "black_gesture_test_support");
