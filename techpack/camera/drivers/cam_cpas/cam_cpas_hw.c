03186cf382fd1d052e589fb7cfb90db569b4f560
pjgowtham <pjgowtham@gmail.com>
2022-05-10 21:54:57 +0530

    techpack/camera: Add changes from GT-ME kernel source

--- techpack/camera/drivers/cam_cpas/cam_cpas_hw.c
+++ techpack/camera/drivers/cam_cpas/cam_cpas_hw.c
@@ -2027,6 +2027,10 @@ static void cam_cpas_update_monitor_array(struct cam_hw_info *cpas_hw,
 		uint32_t be_mnoc_offset =
 			soc_private->rpmh_info[CAM_RPMH_BCM_BE_OFFSET] +
 			(0x4 * soc_private->rpmh_info[CAM_RPMH_BCM_MNOC_INDEX]);
+		//deleted when upgrading code base, check if need restoring
+		//uint32_t be_shub_offset =
+		//	soc_private->rpmh_info[CAM_RPMH_BCM_BE_OFFSET] +
+		//	(0x4 * 1); /* i=1 for SHUB, hardcode for now */
 
 		/*
 		 * 0x4, 0x800 - DDR
@@ -2036,6 +2040,9 @@ static void cam_cpas_update_monitor_array(struct cam_hw_info *cpas_hw,
 		entry->fe_mnoc = cam_io_r_mb(rpmh_base + fe_mnoc_offset);
 		entry->be_ddr = cam_io_r_mb(rpmh_base + be_ddr_offset);
 		entry->be_mnoc = cam_io_r_mb(rpmh_base + be_mnoc_offset);
+        /*deleted when upgrading code base, check if need restoring
+		entry->be_shub = cam_io_r_mb(rpmh_base + be_shub_offset);
+        */
 	}
 
 	entry->camnoc_fill_level[0] = cam_io_r_mb(
@@ -2117,13 +2124,20 @@ static void cam_cpas_dump_monitor_array(
 				entry->axi_info[j].applied_ib_bw,
 				entry->axi_info[j].camnoc_bw);
 		}
-
+        /*deleted when upgrading code base, check if need restoring
 		if (cpas_core->regbase_index[CAM_CPAS_REG_RPMH] != -1) {
+			CAM_INFO(CAM_CPAS,
+				"fe_ddr=0x%x, fe_mnoc=0x%x, be_ddr=0x%x, be_mnoc=0x%x, be_shub=0x%x",
+				entry->fe_ddr, entry->fe_mnoc,
+				entry->be_ddr, entry->be_mnoc, entry->be_shub);
+		}
+        */
+        if (cpas_core->regbase_index[CAM_CPAS_REG_RPMH] != -1) {
 			CAM_INFO(CAM_CPAS,
 				"fe_ddr=0x%x, fe_mnoc=0x%x, be_ddr=0x%x, be_mnoc=0x%x",
 				entry->fe_ddr, entry->fe_mnoc,
 				entry->be_ddr, entry->be_mnoc);
-		}
+        }
 
 		CAM_INFO(CAM_CPAS,
 			"CAMNOC REG[Queued Pending] linear[%d %d] rdi0_wr[%d %d] ubwc_stats0[%d %d] ubwc_stats1[%d %d] rdi1_wr[%d %d]",
